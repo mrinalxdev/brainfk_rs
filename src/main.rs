f2c9c7aff4543f9587f6ad209bd22a112184fcec
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-04-23 11:59:16 +0530

    feat: Multi line text input

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
-use iced::{widget::text, Element, Sandbox, Settings};
+use iced::{widget::{container,text_editor}, Element, Sandbox, Settings};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content : text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action)
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
-        
+        Self {
+            content : text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
-        String::from("A cool editor")
+        String::from("TextFlow : txt editor built by @Mrinal")
     }
     
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
     
     fn view(&self) -> Element<'_, Message> {
-        text("Hello, Iced").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        container(input).padding(10).into()
     }
 
 }
