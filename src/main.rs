72d6c21e22c8ec10c59923f69644e308f3196b14
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-04-23 11:29:41 +0530

    First hello iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Element, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+        
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool editor")
+    }
+    
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+    
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello, Iced").into()
+    }
+
 }
